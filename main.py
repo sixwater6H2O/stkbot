775c98754c824c8d7893f14c409c890ea0d87710
SixWater <62125668+sixwater6H2O@users.noreply.github.com>
2025-04-01 16:02:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ while (i):
     msg = "倒计时：" + str(i)
     pushdeer.send_text("倒计时", desp=msg)
     i-=1
-    time.sleep(1)
+    time.sleep(60)
     
 # pushdeer.send_markdown("# hello world", desp="**optional** description in markdown")
 # pushdeer.send_image("https://github.com/easychen/pushdeer/raw/main/doc/image/clipcode.png")
