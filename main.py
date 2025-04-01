080c9758c8a7609ba1e858991923d323854f5c5f
SixWater <62125668+sixwater6H2O@users.noreply.github.com>
2025-04-01 20:15:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,7 @@
 from pypushdeer import PushDeer
 import os
-import time
 push_key = os.environ["PUSHDEER"]
-pushdeer = PushDeer(pushkey=push_key)
-
-##测试存活时间
-i = 10
-while (i):
-    msg = "倒计时：" + str(i)
-    pushdeer.send_text("倒计时", desp=msg)
-    i-=1
-    time.sleep(60)
-    
-# pushdeer.send_markdown("# hello world", desp="**optional** description in markdown")
-# pushdeer.send_image("https://github.com/easychen/pushdeer/raw/main/doc/image/clipcode.png")
-# pushdeer.send_image(
-#     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVQYV2NgYAAAAAMAAWgmWQ0AAAAASUVORK5CYII=")
+ssycyx = os.environ["SSYCYX"]
+cps_id = os.environ["CPS_id"]
+cps_name = os.environ["CPS_name"]
+print(cps_id,type(cps_id))
