94dfc56eca9cbcb2d8de550289de9c7ee34958f6
SixWater <62125668+sixwater6H2O@users.noreply.github.com>
2025-04-01 20:23:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from pypushdeer import PushDeer
 import os
 push_key = os.environ["PUSHDEER"]
 ssycyx = os.environ["SSYCYX"]
-cps_id = os.environ["CPS_id"]
-cps_name = os.environ["CPS_name"]
-print(cps_id,type(cps_id))
+cps = os.environ["CPS_ID"]
+print(cps,type(cps))
